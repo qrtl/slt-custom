3726e0c8e56b47b3c33b0027795745998601b546
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-03-29 09:06:36 +0800

    [IMP] use hook method

--- website_contract_ext/models/__init__.py
+++ website_contract_ext/models/__init__.py
@@ -2,4 +2,4 @@
 # Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from . import sale_order
+from . import sale_order_hook_action_confirm
