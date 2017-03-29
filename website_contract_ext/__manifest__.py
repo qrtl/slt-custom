88acaf6fb0269e85a0c6b9d01ae5032350548bb8
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-03-29 09:08:32 +0800

    Propose contract template (#2)
    * [ADD] website_contract_ext
    
    * [IMP] use hook method

---
+++ website_contract_ext/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': "Extension to Website Contract",
+    'version': "10.0.1.0.0",
+    'author': "Rooms For (Hong Kong) Limited T/A OSCG",
+    'website': "https://www.odoo-asia.com/",
+    'category': "Website",
+    'license': "LGPL-3",
+    'depends': [
+        'website_contract',
+    ],
+    'data': [
+    ],
+    "application": False,
+    "installable": True,
+}
